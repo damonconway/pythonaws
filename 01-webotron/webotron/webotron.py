24a722523572a1aff31597147f92d9c641acb83c
Damon Conwaym <vstraylight@gmail.com>
2018-10-30 10:15:35 -0700

    Allow user to set AWS profile

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -17,14 +17,23 @@ import click
 
 from bucket import BucketManager
 
-SESSION = boto3.Session(profile_name='pythonAutomation')
-BUCKET_MANAGER = BucketManager(SESSION)
+SESSION = None
+BUCKET_MANAGER = None
 
 
 @click.group()
-def cli():
+@click.option('--profile', default=None,
+    help = "Use a given AWS profile.")
+def cli(profile):
     """Webotron deploys websites to AWS."""
-    pass
+    global SESSION, BUCKET_MANAGER
+
+    session_cfg = {}
+    if profile:
+        session_cfg['profile_name'] = profile
+
+    SESSION = boto3.Session(**session_cfg)
+    BUCKET_MANAGER = BucketManager(SESSION)
 
 
 @cli.command('list-buckets')
